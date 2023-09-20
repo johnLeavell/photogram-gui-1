a27cf5f9a8fc382082f8e2b07d45b506f3ae7a10
johnLeavell <johnleavell@gmail.com>
2023-09-20 14:33:10 -0500

    added create, update, destroy, show for photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,5 +2,59 @@ class PhotosController < ApplicationController
   def index
     @photos = Photo.all.order(created_at: :desc)
   end
-  
+
+  def create
+    @photo = Photo.new
+    @photo.image = params.fetch("input_image")
+    @photo.caption = params.fetch("input_caption")
+    @photo.owner_id = params.fetch("input_owner_id")
+
+    if @photo.valid?
+      @photo.save
+
+      flash[:notice] = "Photo created successfully."
+
+      redirect_to("/photos/#{@photo.id}")
+      
+    else 
+      flash[:notice] = "Photo failed to crate successfully."
+      redirect_to photos_path
+    end
+  end
+
+  def show
+    id = params.fetch("id")
+    @photo = Photo.find_by(id: id)
+
+  end
+
+  def update
+    id = params.fetch("id")
+    @photo = Photo.find_by(id: id)
+
+    @photo.image = params.fetch("input_image")
+    @photo.caption = params.fetch("input_caption")
+
+    if @photo.valid?
+      @photo.save
+
+      flash[:notice] = "Photo updated successfuly."
+
+      redirect_to("/photos/#{@photo.id}")
+    else 
+      flash[:notice] = "Unable to update photo, please try again."
+      redirect_to("/photo/#{@photo.id}")
+    end
+  end
+
+  def destroy
+    id = params.fetch("id")
+    @photo = Photo.find_by(id: id)
+    @photo.destroy
+
+    flash[:notice] = "Photo successfully deleted."
+    
+    redirect_to photos_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   post '/add_user', to: 'users#add_user'
 
   get '/photos', to: 'photos#index'
+  post '/insert_photo_record', to: 'photos#create'
+  get '/photos/:id', to: 'photos#show'
+  post '/update_photo/:id', to: 'photos#update'
+  get '/delete_photo/:id', to: 'photos#destroy'
 end
