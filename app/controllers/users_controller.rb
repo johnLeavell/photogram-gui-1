fef9aa104a5b66f1a32e5ac9e2b7a827beb29e43
johnLeavell <johnleavell@gmail.com>
2023-09-20 13:34:49 -0500

    completed users added photos form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,36 @@ class UsersController < ApplicationController
 
     if @user.valid?
       @user.save
+
+      flash[:notice] = "User created successfully."
+
+      redirect_to("/users/#{@user.username}")
+    else
+      flash[:notice] = "User failed to create successfully."
+
       redirect_to users_path
     end
   end
+
+  def show
+    username = params.fetch("username")
+    @user = User.where({:username => username }).first
+  end
+
+  def update
+      id = params.fetch("id")
+      @user = User.where({ :id => id }).first
+  
+      @user.username = params.fetch("input_username")
+
+  
+      if @user.valid?
+        @user.save
+
+        redirect_to("/users/#{@user.username}")
+      else
+        redirect_to users_path
+      end
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-
+  get '/', to: 'users#index'
   get '/users', to: 'users#index'
+  get '/users/:username', to: 'users#show'
+  post '/update_user/:id', to: 'users#update'
   post '/add_user', to: 'users#add_user'
+
+  get '/photos', to: 'photos#index'
 end
